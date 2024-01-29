d43bf74e9ad15cca24ea4f65f4cc111970895540
ramkoteswarao <117813050+ramkoteswarao@users.noreply.github.com>
2024-01-29 22:22:42 +0530

    Add files via upload

---
+++ Control-Flow--Class-8-CipherSchools-main/Car_8.java
@@ -0,0 +1,26 @@
+class car{
+    String name;
+    String color;
+    int year;
+    int maxspeed;
+    void accelerate(){
+        System.out.println("car is accealerating");
+    }
+}
+public class Car_8{
+    public static void main(String[] args) {
+        car polo=new car();
+        System.out.println(polo.name="honda");
+        System.out.println(polo.color);
+        System.out.println(polo.year);
+        System.out.println(polo.year);
+        polo.name="suzuki";
+        polo.color="red";
+        polo.year=2016;
+        polo.maxspeed=120;
+        System.out.println(polo.name="honda");
+        System.out.println(polo.color);
+        System.out.println(polo.year);
+        System.out.println(polo.year);
+    }
+}
